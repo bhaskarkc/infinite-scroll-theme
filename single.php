2c9d23ab0659b3484c87859076e49460b5b9622c
Bhaskar <bhaskar.kc@domain.com.au>
2018-10-10 22:43:43 +1100

    Home url.

--- single.php
+++ single.php
@@ -3,8 +3,8 @@ get_header();
 
 while ( have_posts() ) {
 	the_post(); ?>
-
-<h1><?php the_title(); ?> </h1>
+<h1><a href="<?php echo get_home_url(); ?>">Home</a></h1>
+<h2><?php the_title(); ?> </h2>
 
 <?php the_post_thumbnail(); ?>
 
